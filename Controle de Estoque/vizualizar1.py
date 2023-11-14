49d7a0be4aa1ec1082438e38a517f17e7641d4c4
Yuri Leal da Cruz <100494057+yurilealdacruz@users.noreply.github.com>
2023-11-14 11:04:36 -0300

    Adicionando Vizualizador

--- Controle de Estoque/vizualizar1.py
+++ Controle de Estoque/vizualizar1.py
@@ -1,2 +1,14 @@
-import customtkinter as tk
+import customtkinter as ctk
 
+janela = ctk.CTk()
+
+
+
+
+
+
+
+
+janela.title("Vizualizar e Editar o Estoque")
+janela.geometry("700x350")
+janela.mainloop()
